7c01da1c49c58415695145bf4e71fe57d0de6d5d
Shikhil Rane <shikhilrane04@gmail.com>
2026-01-10 01:49:16 +0530

    added emtity lifecycyle, manager and persistence context

---
+++ _03_Hibernate_and_Spring_Boot_Data_JPA/_06_Hibernate_Entity_Lifecycle_Entity_Manager_and_Persistence_Context/learnProjection 2/src/main/java/com/shikhilrane/projection/learnProjection/LearnProjectionApplication.java
@@ -0,0 +1,13 @@
+package com.shikhilrane.projection.learnProjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnProjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnProjectionApplication.class, args);
+	}
+
+}
---
+++ _03_Hibernate_and_Spring_Boot_Data_JPA/_06_Hibernate_Entity_Lifecycle_Entity_Manager_and_Persistence_Context/learnProjection 2/src/main/java/com/shikhilrane/projection/learnProjection/entities/Patient.java
@@ -0,0 +1,35 @@
+package com.shikhilrane.projection.learnProjection.entities;
+
+import com.shikhilrane.projection.learnProjection.entities.type.BloodGroupType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private LocalDate birthDate;
+
+    private String email;
+
+    private String gender;
+
+    @Enumerated(value = EnumType.STRING)
+    private BloodGroupType bloodGroup;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
