342f65320ff46b90ca39964b0d4e6365dfaf6311
Shikhil Rane <shikhilrane04@gmail.com>
2026-01-11 21:57:56 +0530

    added dept

---
+++ _03_Hibernate_and_Spring_Boot_Data_JPA/_07_Entity_Relationship_Mapping_in_Spring_Boot/ERD/src/main/java/com/shikhilrane/projection/learnProjection/entities/Department.java
@@ -0,0 +1,23 @@
+package com.shikhilrane.projection.learnProjection.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+
+
+}
