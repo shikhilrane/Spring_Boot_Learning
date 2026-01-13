34200949237a7c725fefdb4a4d808b8231971273
Shikhil Rane <shikhilrane04@gmail.com>
2026-01-13 21:17:30 +0530

    Added understanding in code

--- _03_Hibernate_and_Spring_Boot_Data_JPA/_08_Cascading_Types_and_Transactional_in_Relational_Queries/Cascading_and_Transactional/src/main/java/com/shikhilrane/projection/learnProjection/services/InsuranceService.java
+++ _03_Hibernate_and_Spring_Boot_Data_JPA/_08_Cascading_Types_and_Transactional_in_Relational_Queries/Cascading_and_Transactional/src/main/java/com/shikhilrane/projection/learnProjection/services/InsuranceService.java
@@ -2,7 +2,6 @@ package com.shikhilrane.projection.learnProjection.services;
 
 import com.shikhilrane.projection.learnProjection.entities.Insurance;
 import com.shikhilrane.projection.learnProjection.entities.Patient;
-import com.shikhilrane.projection.learnProjection.repositories.InsuranceRepository;
 import com.shikhilrane.projection.learnProjection.repositories.PatientRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -11,14 +10,13 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class InsuranceService {
-    private final InsuranceRepository insuranceRepository;
-    private final PatientRepository patientRepository;
+    private final PatientRepository patientRepository;  // add PatientRepository, So that we can fetch data of patient from PatientRepository
 
-    @Transactional
-    public Insurance assignInsuranceToPatient(Insurance insurance, Long patientId){
-        Patient patient = patientRepository.findById(patientId).orElseThrow();
-        patient.setInsurance(insurance);
-        insurance.setPatient(patient);
-        return insurance;
+    @Transactional // Its important to have @Transactional, if we don't have every fetching and saving opn will have their own persistence context but we add then every operation from given method will happen in single persistence context. Also, if some operation fails then this will roll the method
+    public Insurance assignInsuranceToPatient(Insurance insurance, Long patientId){ // Created Insurance will be assigned to entered patientId
+        Patient patient = patientRepository.findById(patientId).orElseThrow();      // Find the patientId first or throw not found
+        patient.setInsurance(insurance);                                            // If patientId matches then Insurance will be assign to that patientId
+        insurance.setPatient(patient);                                              // Optional but good for bidirectional mapping consistency
+        return insurance;                                                           // It will return the insurance according to CascadeType
     }
 }
