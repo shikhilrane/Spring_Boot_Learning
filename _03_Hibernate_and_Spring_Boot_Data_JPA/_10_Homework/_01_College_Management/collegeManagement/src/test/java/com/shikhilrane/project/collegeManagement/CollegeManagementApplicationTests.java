c50a8b29ac3c5b4307fbf7f29638c112b2bebcfc
Shikhil Rane <shikhilrane04@gmail.com>
2026-01-22 14:37:14 +0530

    added project for student management system

---
+++ _03_Hibernate_and_Spring_Boot_Data_JPA/_10_Homework/_01_College_Management/collegeManagement/src/test/java/com/shikhilrane/project/collegeManagement/CollegeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shikhilrane.project.collegeManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
