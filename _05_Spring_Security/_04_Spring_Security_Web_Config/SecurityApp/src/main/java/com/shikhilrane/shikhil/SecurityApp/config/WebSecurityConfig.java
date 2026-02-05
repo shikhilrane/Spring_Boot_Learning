71f2a83b2a77ea67eeea54242aca393e7dca2c53
Shikhil Rane <shikhilrane04@gmail.com>
2026-02-05 20:58:56 +0530

    added WebConfig

---
+++ _05_Spring_Security/_04_Spring_Security_Web_Config/SecurityApp/src/main/java/com/shikhilrane/shikhil/SecurityApp/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.shikhilrane.shikhil.SecurityApp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .formLogin(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+}
