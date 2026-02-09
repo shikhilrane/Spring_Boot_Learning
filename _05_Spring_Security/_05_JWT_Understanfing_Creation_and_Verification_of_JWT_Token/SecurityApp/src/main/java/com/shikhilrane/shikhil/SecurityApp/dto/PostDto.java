07abeed8f93ebba0e1c3365b8cc9836946b329e9
Shikhil Rane <shikhilrane04@gmail.com>
2026-02-09 21:02:22 +0530

    added JWT Creation

---
+++ _05_Spring_Security/_05_JWT_Understanfing_Creation_and_Verification_of_JWT_Token/SecurityApp/src/main/java/com/shikhilrane/shikhil/SecurityApp/dto/PostDto.java
@@ -0,0 +1,13 @@
+package com.shikhilrane.shikhil.SecurityApp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostDto {
+    private String title;
+    private String description;
+}
