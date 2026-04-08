04dde54bfb485d5ce2781b5dc5dccccbf065b469
Shikhil Rane <shikhilrane04@gmail.com>
2026-04-08 15:55:12 +0530

    added access token and refresh token

---
+++ _05_Spring_Security/_11_Access_token_and_Refresh_token/SecurityApp/src/main/java/com/shikhilrane/shikhil/SecurityApp/filters/LoggingFilter.java
@@ -0,0 +1,22 @@
+package com.shikhilrane.shikhil.SecurityApp.filters;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class LoggingFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("Incoming Request -> Method: {}, URI: {}", request.getMethod(), request.getRequestURI()); // Log HTTP method and requested endpoint
+        filterChain.doFilter(request, response);                                                           // Pass request to next filter in the filter chain
+        log.info("Response Status -> {}", response.getStatus());                                           // Log response status after request processing
+    }
+}
---
+++ _05_Spring_Security/_11_Access_token_and_Refresh_token/SecurityApp/src/main/java/com/shikhilrane/shikhil/SecurityApp/services/Implementation/MyUserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.shikhilrane.shikhil.SecurityApp.services.Implementation;
+
+import com.shikhilrane.shikhil.SecurityApp.entities.User;
+import com.shikhilrane.shikhil.SecurityApp.exceptions.ResourceNotFoundException;
+import com.shikhilrane.shikhil.SecurityApp.repositories.UserRepository;
+import com.shikhilrane.shikhil.SecurityApp.services.MyUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MyUserDetailsServiceImpl implements MyUserDetailsService {
+
+    private final UserRepository userRepository;
+
+    // This method is for login using JWT
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository
+                .findByEmail(username)
+                .orElseThrow(() -> new BadCredentialsException("User with entered email " + username + " not found"));
+    }
+
+    // This method is for Custom Filter for authentication
+    public User getUserById(Long userId){
+        return userRepository
+                .findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User with entered email " + userId + " not found"));
+    }
+}
