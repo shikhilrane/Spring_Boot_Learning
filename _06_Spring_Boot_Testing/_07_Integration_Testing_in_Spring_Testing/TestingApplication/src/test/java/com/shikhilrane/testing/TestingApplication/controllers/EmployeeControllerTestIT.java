62fca123fe30babe22f0e930556267909d46e305
Shikhil Rane <shikhilrane04@gmail.com>
2026-04-20 21:20:40 +0530

    added more in Integration Testing

---
+++ _06_Spring_Boot_Testing/_07_Integration_Testing_in_Spring_Testing/TestingApplication/src/test/java/com/shikhilrane/testing/TestingApplication/controllers/EmployeeControllerTestIT.java
@@ -0,0 +1,10 @@
+package com.shikhilrane.testing.TestingApplication.controllers;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class EmployeeControllerTestIT {
+
+}
