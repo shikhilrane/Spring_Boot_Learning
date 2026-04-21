5a807ffd799cae1c28476e845b6229f09400df3e
Shikhil Rane <shikhilrane04@gmail.com>
2026-04-21 21:16:11 +0530

    added configuration for integration testing

--- _06_Spring_Boot_Testing/_07_Integration_Testing_in_Spring_Testing/TestingApplication/src/test/java/com/shikhilrane/testing/TestingApplication/controllers/AbstractIntegrationTest.java
+++ _06_Spring_Boot_Testing/_07_Integration_Testing_in_Spring_Testing/TestingApplication/src/test/java/com/shikhilrane/testing/TestingApplication/controllers/AbstractIntegrationTest.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-@AutoConfigureWebTestClient(timeout = "100000") // Enables and configures WebTestClient for integration testing of web endpoints with a custom timeout
+@AutoConfigureWebTestClient(timeout = "100000") // Enables and configures WebTestClient for integration testing and sets maximum wait time for API response so the test doesn't fail if the response takes longer
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // Loads the full Spring Boot application context and starts the server on a random port for integration testing
 @Import(TestContainerConfiguration.class) // Imports TestContainerConfiguration so that the PostgreSQL Testcontainer is started and used during tests
 public class AbstractIntegrationTest {
--- _06_Spring_Boot_Testing/_07_Integration_Testing_in_Spring_Testing/TestingApplication/src/test/java/com/shikhilrane/testing/TestingApplication/controllers/EmployeeControllerTestIT.java
+++ _06_Spring_Boot_Testing/_07_Integration_Testing_in_Spring_Testing/TestingApplication/src/test/java/com/shikhilrane/testing/TestingApplication/controllers/EmployeeControllerTestIT.java
@@ -1,6 +1,7 @@
 package com.shikhilrane.testing.TestingApplication.controllers;
 
 import com.shikhilrane.testing.TestingApplication.repositories.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,5 +10,9 @@ class EmployeeControllerTestIT extends AbstractIntegrationTest {
     @Autowired
     private EmployeeRepository employeeRepository;  // Used to save or delete employee data in the database before running the API tests
 
-    
+    @BeforeEach
+    void setUp() {  // Runs before every test and clears the database so that each test starts with a clean state and previous test data does not affect other tests
+        employeeRepository.deleteAll();
+    }
+
 }
