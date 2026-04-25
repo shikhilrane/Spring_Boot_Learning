b46c3697bdc3cd4f7ac3adb07f23122e3f6537ab
Shikhil Rane <shikhilrane04@gmail.com>
2026-04-25 19:18:42 +0530

    added git profiles completely

--- _07_Spring_Boot_Deployment_with_CI_CD/_02_Spring_Profiles_Creating_dev_and_prod_environments_in_SpringBoot/TestingApplication_3.5.13_Version/src/main/java/com/shikhilrane/testing/TestingApplication/services/impl/DataServiceImplDev.java
+++ _07_Spring_Boot_Deployment_with_CI_CD/_02_Spring_Profiles_Creating_dev_and_prod_environments_in_SpringBoot/TestingApplication_3.5.13_Version/src/main/java/com/shikhilrane/testing/TestingApplication/services/impl/DataServiceImplDev.java
@@ -1,7 +1,11 @@
 package com.shikhilrane.testing.TestingApplication.services.impl;
 
 import com.shikhilrane.testing.TestingApplication.services.DataService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
+@Service
+@Profile("dev")
 public class DataServiceImplDev implements DataService {
     @Override
     public String getData() {
