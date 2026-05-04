38ac8cc6334956186f8f6a77e9245ab10f5101ea
Shikhil Rane <shikhilrane04@gmail.com>
2026-05-04 21:28:34 +0530

    Default caching in Springboot added

---
+++ _09_Caching_and_Concurrent_Transaction_Management/_02_Default_Caching_in_Springboot/TestingApplication_3.5.13_Version/src/main/java/com/shikhilrane/testing/TestingApplication/config/CachingConfig.java
@@ -0,0 +1,8 @@
+package com.shikhilrane.testing.TestingApplication.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CachingConfig {}
